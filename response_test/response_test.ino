3c64e1f73b9be317301b93834177493420e81bb2
zhanyanchen8 <zhanyanchen8@gmail.com>
2019-08-09 21:11:04 -0400

    add parsing of one-line parsing for data extraction

--- response_test/response_test.ino
+++ response_test/response_test.ino
@@ -7,9 +7,27 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   String jetson = Serial.readString();
-  //Serial.write(jetson); //test
   Serial.write(jetson);
-  // String fromJetson = (String)(jetson);
+
+  int index = 0;
+  int source = jetson.indexOf('S', index) + 2;
+  index = source;
+
+  int dest = jetson.indexOf('D', index) + 2;
+  index = dest;
+
+  int id = jetson.indexOf('T', jetson.indexOf("MOTOR") + "MOTOR".length()) + 2;
+  index = id;
+
+  int data = jetson.indexOf('P', index) + 2;
+  index = data;
+  
+  String sourceName = jetson.substring(source, dest - 2);
+  String destName = jetson.substring(dest, id - 2);
+  String idName = jetson.substring(id, data - 2);
+  data = String.toInt(jetson.substring(data, jetson.lastIndexOf("K")));
+
+  Serial.write(destName + "/" + data);
   
   /**
   // drivetrain motor
@@ -19,12 +37,6 @@ void loop() {
   // arm motor
   else {
     
-  }
-  */
-  /**
-  for (byte n = 0; n < 255; n++){
-    Serial.write(n);
-    delay(50);
   }
   */
 }
